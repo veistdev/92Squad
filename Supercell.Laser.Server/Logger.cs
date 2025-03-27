4406103f86f41b154796afe6890f66447e136efc
veistdv <dera.veistjr@mail.ru>
2025-03-28 00:52:54 +0300

    Add files via upload

---
+++ Supercell.Laser.Server/Logger.cs
@@ -0,0 +1,77 @@
+﻿namespace Supercell.Laser.Server
+{
+    using Supercell.Laser.Titan.Debug;
+    using System;
+    using System.Text;
+
+    public static class Logger
+    {
+
+        private static StringBuilder FileLogger;
+        public static void Print(string log)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("[DEBUG] " + log);
+        }
+
+        public static void Init()
+        {
+            FileLogger = new StringBuilder();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Debugger.SetListener(new DebuggerListener());
+        }
+
+        public static void Warning(string log)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("[WARNING] " + log);
+        }
+
+        public static void Error(string log)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("[ERROR] " + log);
+        }
+        public static void Log(string log)
+        {
+            FileLogger.Append($"[{DateTime.Now}][WARN!!] " + log + "\n");
+            File.AppendAllText("./log.txt", FileLogger.ToString());
+            FileLogger.Clear();
+        }
+
+        public static void BLog(string log)
+        {
+            FileLogger.Append($"[{DateTime.Now}] " + log + "\n");
+            File.AppendAllText("./battles.txt", FileLogger.ToString());
+            FileLogger.Clear();
+        }
+
+        public static void HandleReport(string log)
+        {
+            FileLogger.Append($"[{DateTime.Now}] " + log + "\n");
+            File.AppendAllText("./chatreports.txt", FileLogger.ToString());
+            FileLogger.Clear();
+        }
+    }
+
+    public class DebuggerListener : IDebuggerListener
+    {
+        public void Error(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("[LOGIC] Error: " + message);
+        }
+
+        public void Print(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("[LOGIC] Info: " + message);
+        }
+
+        public void Warning(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("[LOGIC] Warning: " + message);
+        }
+    }
+}
---
+++ Supercell.Laser.Server/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Supercell.Laser.Server
+{
+    using Supercell.Laser.Server.Handler;
+    using Supercell.Laser.Server.Settings;
+    using System.Drawing;
+
+    static class Program
+    {
+        public const string SERVER_VERSION = "1.2";
+        public const string BUILD_TYPE = "Beta";
+
+        private static void Main(string[] args)
+        {
+            Console.Title = "92Squad - server emulator";
+            Directory.SetCurrentDirectory(AppContext.BaseDirectory);
+
+            Colorful.Console.WriteWithGradient(
+                @"
+
+░█████╗░██████╗░  ░██████╗░██████╗░██╗░░░██╗░█████╗░██████╗░
+██╔══██╗╚════██╗  ██╔════╝██╔═══██╗██║░░░██║██╔══██╗██╔══██╗
+╚██████║░░███╔═╝  ╚█████╗░██║██╗██║██║░░░██║███████║██║░░██║
+░╚═══██║██╔══╝░░  ░╚═══██╗╚██████╔╝██║░░░██║██╔══██║██║░░██║
+░█████╔╝███████╗  ██████╔╝░╚═██╔═╝░╚██████╔╝██║░░██║██████╔╝
+░╚════╝░╚══════╝  ╚═════╝░░░░╚═╝░░░░╚═════╝░╚═╝░░╚═╝╚═════╝░                                           " + "\n\n\n", Color.Fuchsia, Color.Cyan, 8);
+
+            Logger.Print("Server By: 92Squad");
+
+            Logger.Init();
+            Configuration.Instance = Configuration.LoadFromFile("config.json");
+
+            Resources.InitDatabase();
+            Resources.InitLogic();
+            Resources.InitNetwork();
+
+            Logger.Print("Server started!");
+
+            ExitHandler.Init();
+            CmdHandler.Start();
+        }
+    }
+}
